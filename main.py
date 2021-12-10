a1da05ca4bf3ca2f10fd8f349b2742201d974d5b
whosminatti <minatti.leandro@gmail.com>
2021-12-10 13:43:47 -0300

    Rock, Paper, Scissors developed using Python

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+
+def play():
+    user = input("What's yout choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+    computer = random.choice(['r','p','s'])
+
+    if user == computer:
+        return 'It\'s a tie'
+    # r > s, s > p, p > r
+    if is_win(user, computer):
+        return 'You won!'
+
+    return 'You lose!'
+
+def is_win(player, opponent):
+    # returns true if player wins
+    # r > s, s > p, p > r
+
+    if(player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+print(play())
